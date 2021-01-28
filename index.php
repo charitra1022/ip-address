c3684c66ed332769130166c6ccc516352b777a88
Charitra Agarwal <charitraagarwal1022@gmail.com>
2021-01-28 16:30:36 +0530

    used php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>IP Address</title>
+    </head>
+
+    <body>
+        <p id="ip-address"></p>
+        <?php
+            function getUserIpAddr(){
+                if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+                    //ip from share internet
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+                }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+                    //ip pass from proxy
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                }else{
+                    $ip = $_SERVER['REMOTE_ADDR'];
+                }
+                return $ip;
+            }
+            
+            echo 'User Real IP - '.getUserIpAddr();        
+        ?>
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </body>
+</html>
